8654025eb46ffd7234920ade5a910552f0bdfa5b
wjunjie01 <junjiework48@gmail.com>
2024-02-23 20:20:06 +0800

    Add notification message in print all data

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -38,6 +38,7 @@ public class Main {
     }
 
     public static void printAllData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data using iteration... ");
         for (Task t : tasksData) {
             System.out.println(t);
         }
